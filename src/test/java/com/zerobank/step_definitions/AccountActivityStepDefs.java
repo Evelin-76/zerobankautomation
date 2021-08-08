975304ec4e0b824519621a15e740cb661858424c
Evelin <evelindacanovas@gmail.com>
2021-08-08 14:51:38 +0200

    AccountActivityStepDefs checking default drop dawn menu option verified and done

--- src/test/java/com/zerobank/step_definitions/AccountActivityStepDefs.java
+++ src/test/java/com/zerobank/step_definitions/AccountActivityStepDefs.java
@@ -1,11 +1,13 @@
 package com.zerobank.step_definitions;
 
+import com.zerobank.pages.AccountActivityPage;
 import com.zerobank.pages.DashboardPage;
 import com.zerobank.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 
 public class AccountActivityStepDefs {
     @And("user is in Dashboard page")
@@ -20,13 +22,17 @@ public class AccountActivityStepDefs {
     }
 
     @Then("drop down default option is {string}")
-    public void dropDownDefaultOptionIs(String expectedPageTitle) {
-        String actualPageTitle = Driver.get().getTitle();
-        Assert.assertEquals(expectedPageTitle,actualPageTitle);
+    public void dropDownDefaultOptionIs(String expectedDefaultOptionName) {
+     //   System.out.println(Driver.get().findElement(By.xpath("//option[@value ='1']")));
+        String actualDefaultOptionName = new AccountActivityPage().getSingleOptionDropDownText(1);
+        Assert.assertEquals(expectedDefaultOptionName,actualDefaultOptionName);
+        System.out.println(expectedDefaultOptionName);
+        System.out.println(actualDefaultOptionName);
     }
 
     @When("user clicks on drop dawn options box")
     public void userClicksOnDropDawnOptionsBox() {
+        new AccountActivityPage().accountDrowDownBox.click();
     }
 
     @Then("drop down options are:")
