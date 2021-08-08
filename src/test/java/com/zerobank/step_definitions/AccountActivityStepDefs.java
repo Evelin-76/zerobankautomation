0b83af8805691f0a57fc2f2565d3545d42e97c6f
Evelin <evelindacanovas@gmail.com>
2021-08-08 12:47:26 +0200

    AccountActivityStepDefs class created Steps defined

---
+++ src/test/java/com/zerobank/step_definitions/AccountActivityStepDefs.java
@@ -0,0 +1,31 @@
+package com.zerobank.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AccountActivityStepDefs {
+    @And("user is in Dashboard page")
+    public void userIsInDashboardPage() {
+    }
+
+    @When("user navigate to {string}")
+    public void userNavigateTo(String arg0) {
+    }
+
+    @Then("drop down default option is {string}")
+    public void dropDownDefaultOptionIs(String arg0) {
+    }
+
+    @When("user clicks on drop dawn options box")
+    public void userClicksOnDropDawnOptionsBox() {
+    }
+
+    @Then("drop down options are:")
+    public void dropDownOptionsAre() {
+    }
+
+    @Then("Transtaction table column names are:")
+    public void transtactionTableColumnNamesAre() {
+    }
+}
